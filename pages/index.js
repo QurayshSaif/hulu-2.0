4752a90710b4e29b0f128878a49b6efb8b51891e
QurayshSaif <saifquraysh@gmaill.com>
2023-05-03 17:16:13 -0400

    Added header component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <h1>Hulu 2.0</h1>
+      <Header />
     </>
   );
 }
